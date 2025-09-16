06be7913d0ff054b816bee78e10958ca4558d8b1
Ely241 <techelyndra@gmail.com>
2025-09-16 12:57:21 +0200

    fix(backend): Add SSL config to Knex production connection

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -17,7 +17,12 @@ const config: { [key: string]: Knex.Config } = {
 
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    },
     migrations: {
       directory: "./src/database/migrations"
     },
